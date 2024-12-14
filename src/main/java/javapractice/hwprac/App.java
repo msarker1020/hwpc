3e67631c1e94d6d47c756d7049fa48b943da569a
msarker1020 <muhammad.sagar4582@gmail.com>
2024-12-13 19:44:44 -0500

    UpP

---
+++ src/main/java/javapractice/hwprac/App.java
@@ -0,0 +1,13 @@
+package javapractice.hwprac;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
